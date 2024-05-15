f91be0e4d2a56c724e0ed163b1f83af7e28cad9f
philippart-s <wildagsx@gmail.com>
2024-05-15 17:38:36 +0200

    feat: 🖼️ manage images

--- ai-in-few-minutes/cncf-lorient-2024/slidesk.js
+++ ai-in-few-minutes/cncf-lorient-2024/slidesk.js
@@ -3,7 +3,7 @@
       currentSlide: 0,
       slides: [],
       animationTimer: 300,
-      onSlideChange: function() {window.slidesk.prepareSteps();;window.slidesk.progressActive();;;window.slidesk.sbs();;window.slidesk.animateElements();},
+      onSlideChange: function() {window.slidesk.prepareSteps();;window.slidesk.progressActive();;window.slidesk.animateElements();;;window.slidesk.sbs();},
       env: {"PLUGINS":"steps, progress","WIDTH":"1920"},
       cwd: '/Users/sphilipp/Documents/Talks/ai-in-few-minutes-slides/',
       lastAction: ""
